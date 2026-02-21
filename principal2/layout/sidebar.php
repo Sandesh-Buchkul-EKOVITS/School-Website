eefffdf007022686abb5bf66c331137c45317a27
Gayatriwaghmare1 <145859414+Gayatriwaghmare1@users.noreply.github.com>
2026-02-22 00:14:38 +0530

    add files by gayatri

---
+++ principal2/layout/sidebar.php
@@ -0,0 +1,45 @@
+<?php
+$current = $_GET['page'] ?? 'dashboard';
+?>
+
+<div class="sidebar">
+
+<ul class="menu">
+
+    <li class="<?= ($current=='dashboard') ? 'active' : '' ?>">
+        <a href="/SCHOOL-WEBSITE/principal2/index.php?page=dashboard">📊 <span>Dashboard</span></a>
+    </li>
+
+    <li class="<?= ($current=='students') ? 'active' : '' ?>">
+        <a href="/SCHOOL-WEBSITE/principal2/index.php?page=students">👨‍🎓 <span>Students</span></a>
+    </li>
+
+    <li class="<?= ($current=='teachers') ? 'active' : '' ?>">
+        <a href="/SCHOOL-WEBSITE/principal2/index.php?page=teachers">👩‍🏫 <span>Teachers</span></a>
+    </li>
+
+    <li class="<?= ($current=='attendance') ? 'active' : '' ?>">
+        <a href="/SCHOOL-WEBSITE/principal2/index.php?page=attendance">📅 <span>Attendance</span></a>
+      
+
+    </li>
+
+    <li class="<?= ($current=='fees') ? 'active' : '' ?>">
+        <a href="/SCHOOL-WEBSITE/principal2/index.php?page=fees">💰 <span>Fees</span></a>
+    </li>
+
+    <li class="<?= ($current=='exams') ? 'active' : '' ?>">
+        <a href="/SCHOOL-WEBSITE/principal2/index.php?page=exams">📝 <span>Exams</span></a>
+    </li>
+
+    <li class="<?= ($current=='notice') ? 'active' : '' ?>">
+        <a href="/SCHOOL-WEBSITE/principal2/index.php?page=notice">🔔 <span>Notices</span></a>
+    </li>
+
+      <li class="<?= ($current=='enquires') ? 'active' : '' ?>">
+        <a href="/index.php?page=enquires">🔔 <span>Enquires</span></a>
+    </li>
+
+</ul>
+
+</div>
